8d665e53232d2b67f5f4826c3ff6e3a3db37b565
b08203031 <b08203031@ntu.edu.tw>
2025-11-08 00:40:21 +0800

    添加註解

--- chess_engine/move.py
+++ chess_engine/move.py
@@ -7,30 +7,71 @@ import numba as nb
 # Bits | 15, 14       | 13, 12           | 11, 10, 9, 8, 7, 6  | 5, 4, 3, 2, 1, 0
 # Use  | Special Flag | Promotion Piece  | To Square           | From Square
 
-# --- Constants for Bit Manipulation ---
-FROM_SQUARE_MASK = np.uint16(0x003F)  # 0000 0000 0011 1111
-TO_SQUARE_MASK = np.uint16(0x0FC0)    # 0000 1111 1100 0000
-PROMOTION_PIECE_MASK = np.uint16(0x3000) # 0011 0000 0000 0000
-SPECIAL_MOVE_MASK = np.uint16(0xC000) # 1100 0000 0000 0000
+FROM_SQUARE_MASK = np.uint16(0x003F)
+"""
+A bitmask to extract the from_square from a move.
+"""
+TO_SQUARE_MASK = np.uint16(0x0FC0)
+"""
+A bitmask to extract the to_square from a move.
+"""
+PROMOTION_PIECE_MASK = np.uint16(0x3000)
+"""
+A bitmask to extract the promotion piece from a move.
+"""
+SPECIAL_MOVE_MASK = np.uint16(0xC000)
+"""
+A bitmask to extract the special move flag from a move.
+"""
 
 TO_SQUARE_SHIFT = 6
+"""
+The number of bits to shift to get the to_square.
+"""
 PROMOTION_PIECE_SHIFT = 12
+"""
+The number of bits to shift to get the promotion piece.
+"""
 SPECIAL_MOVE_SHIFT = 14
+"""
+The number of bits to shift to get the special move flag.
+"""
 
-# --- Constants for Special Move Flags ---
 SPECIAL_MOVE_FLAG_NORMAL = np.uint16(0)
+"""
+A flag that indicates a normal move.
+"""
 SPECIAL_MOVE_FLAG_PROMOTION = np.uint16(1)
+"""
+A flag that indicates a promotion move.
+"""
 SPECIAL_MOVE_FLAG_EN_PASSANT = np.uint16(2)
+"""
+A flag that indicates an en passant move.
+"""
 SPECIAL_MOVE_FLAG_CASTLING = np.uint16(3)
+"""
+A flag that indicates a castling move.
+"""
 
-# --- Constants for Promotion Pieces ---
-# These values, when shifted, will be stored in the promotion piece bits.
 PROMO_KNIGHT, PROMO_BISHOP, PROMO_ROOK, PROMO_QUEEN = 0, 1, 2, 3
+"""
+Constants for promotion pieces.
+"""
 
 @nb.jit(nopython=True, inline='always')
 def encode_move(from_square: int, to_square: int, promotion_piece: int, special_flag: int) -> np.uint16:
     """
     Encodes move information into a 16-bit unsigned integer.
+
+    Args:
+        from_square: The square the piece is moving from.
+        to_square: The square the piece is moving to.
+        promotion_piece: The piece to promote to.
+        special_flag: The special move flag.
+
+    Returns:
+        A 16-bit unsigned integer representing the move.
     """
     move = np.uint16(from_square)
     move |= np.uint16(to_square << TO_SQUARE_SHIFT)
@@ -40,20 +81,52 @@ def encode_move(from_square: int, to_square: int, promotion_piece: int, special_
 
 @nb.jit(nopython=True, inline='always')
 def get_from_square(move: np.uint16) -> int:
-    """Extracts the from_square from a move."""
+    """
+    Extracts the from_square from a move.
+
+    Args:
+        move: The move to extract the from_square from.
+
+    Returns:
+        The from_square of the move.
+    """
     return int(move & FROM_SQUARE_MASK)
 
 @nb.jit(nopython=True, inline='always')
 def get_to_square(move: np.uint16) -> int:
-    """Extracts the to_square from a move."""
+    """
+    Extracts the to_square from a move.
+
+    Args:
+        move: The move to extract the to_square from.
+
+    Returns:
+        The to_square of the move.
+    """
     return int((move & TO_SQUARE_MASK) >> TO_SQUARE_SHIFT)
 
 @nb.jit(nopython=True, inline='always')
 def get_promotion_piece(move: np.uint16) -> int:
-    """Extracts the promotion piece type from a move."""
+    """
+    Extracts the promotion piece type from a move.
+
+    Args:
+        move: The move to extract the promotion piece from.
+
+    Returns:
+        The promotion piece of the move.
+    """
     return int((move & PROMOTION_PIECE_MASK) >> PROMOTION_PIECE_SHIFT)
 
 @nb.jit(nopython=True, inline='always')
 def get_special_move_flag(move: np.uint16) -> int:
-    """Extracts the special move flag from a move."""
+    """
+    Extracts the special move flag from a move.
+
+    Args:
+        move: The move to extract the special move flag from.
+
+    Returns:
+        The special move flag of the move.
+    """
     return int((move & SPECIAL_MOVE_MASK) >> SPECIAL_MOVE_SHIFT)
--- chess_engine/zobrist.py
+++ chess_engine/zobrist.py
@@ -3,29 +3,53 @@ import numpy as np
 import numba as nb
 from chess_engine.engine_types import piece_bbs_signature, game_state_signature
 
-# --- Constants ---
 BLACK = 1
+"""
+A constant for the black color.
+"""
 
-# --- Initialize Zobrist Keys ---
 np.random.seed(123456789)
 PIECE_SQUARE_KEYS = np.random.randint(1, 2**64 - 1, (12, 64), dtype=np.uint64)
+"""
+A 2D array of random numbers for each piece on each square.
+"""
 SIDE_TO_MOVE_KEY = np.random.randint(1, 2**64 - 1, dtype=np.uint64)
+"""
+A random number for the side to move.
+"""
 EN_PASSANT_FILE_KEYS = np.random.randint(1, 2**64 - 1, 8, dtype=np.uint64)
+"""
+A 1D array of random numbers for each en passant file.
+"""
 CASTLING_RIGHTS_KEYS = np.random.randint(1, 2**64 - 1, 16, dtype=np.uint64)
+"""
+A 1D array of random numbers for each castling rights combination.
+"""
 
-# --- De Bruijn Bitscan for LSB ---
 BIT_TO_INDEX = np.array([
     0, 47,  1, 56, 48, 27,  2, 60, 57, 49, 41, 37, 28, 16,  3, 61,
    54, 58, 35, 50, 42, 21, 38, 31, 29, 17, 44,  4, 62, 52, 55, 34,
    59, 36, 55, 51, 43, 22, 39, 32, 30, 18, 45,  5, 63, 53, 33, 23,
    40, 19, 46,  6, 24,  7, 20,  8, 25,  9, 14, 10, 26, 11, 15, 12, 13
 ], dtype=np.uint8)
+"""
+A lookup table for mapping the De Bruijn hash to a square index (0-63).
+"""
 DEBRUIJN_MAGIC = np.uint64(0x03f79d71b4cb0a89)
+"""
+A De Bruijn sequence for fast bit scanning.
+"""
 
 @nb.jit(nb.int8(nb.uint64), nopython=True, inline='always')
 def get_lsb_index(bitboard: np.uint64) -> int:
     """
     Finds the index of the LSB using a De Bruijn sequence bitscan.
+
+    Args:
+        bitboard: The bitboard to find the LSB of.
+
+    Returns:
+        The index of the LSB.
     """
     bitboard = np.uint64(bitboard)
     if bitboard == 0:
@@ -38,11 +62,17 @@ def get_lsb_index(bitboard: np.uint64) -> int:
 def compute_initial_hash(piece_bbs: tuple, game_state: tuple) -> np.uint64:
     """
     Computes the Zobrist hash from scratch.
+
+    Args:
+        piece_bbs: A tuple of 12 bitboards representing the pieces.
+        game_state: A tuple representing the current game state.
+
+    Returns:
+        The Zobrist hash of the position.
     """
     zobrist_key = np.uint64(0)
     side_to_move, castling_rights, en_passant_square, _, _ = game_state
 
-    # 1. Hash Piece-Square Keys
     for piece_type in range(12):
         bb = piece_bbs[piece_type]
         while bb != 0:
@@ -50,15 +80,12 @@ def compute_initial_hash(piece_bbs: tuple, game_state: tuple) -> np.uint64:
             zobrist_key ^= PIECE_SQUARE_KEYS[piece_type, square]
             bb &= np.uint64(bb - 1)
 
-    # 2. Hash Castling Rights
     zobrist_key ^= CASTLING_RIGHTS_KEYS[castling_rights]
 
-    # 3. Hash En Passant Square
     if en_passant_square != -1:
         ep_file = en_passant_square % 8
         zobrist_key ^= EN_PASSANT_FILE_KEYS[ep_file]
 
-    # 4. Hash Side to Move
     if side_to_move == BLACK:
         zobrist_key ^= SIDE_TO_MOVE_KEY
 
