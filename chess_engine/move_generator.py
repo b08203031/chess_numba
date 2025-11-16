3087890d3c6afb0fe69b738dd9d1e3447f8e445d
b08203031 <b08203031@ntu.edu.tw>
2025-11-17 01:14:44 +0800

    commit

--- chess_engine/move_generator.py
+++ chess_engine/move_generator.py
@@ -111,27 +111,27 @@ def init_sliders_attacks():
             magic_index = (np.uint64(occ) * ROOK_MAGIC_NUMBERS[sq]) >> np.uint64(64 - ROOK_RELEVANT_BITS[sq])
             rook_attacks[sq][magic_index] = rook_attacks_on_the_fly(sq, occ)
     return bishop_attacks, rook_attacks
-BISHOP_ATTACKS, ROOK_ATTACKS = init_sliders_attacks()
-# BISHOP_ATTACKS = np.empty((64, 512), dtype=np.uint64)
-# ROOK_ATTACKS = np.empty((64, 4096), dtype=np.uint64)
+# BISHOP_ATTACKS, ROOK_ATTACKS = init_sliders_attacks()
+BISHOP_ATTACKS = np.empty((64, 512), dtype=np.uint64)
+ROOK_ATTACKS = np.empty((64, 4096), dtype=np.uint64)
 
 @numba.njit(numba.uint64(numba.uint8, numba.uint64), cache=True, boundscheck=False, fastmath=True)
 def get_bishop_attacks(sq, occ):
     """
     使用魔法位元棋盤查表獲取主教的攻擊。
     """
-    occ &= BISHOP_MASKS[sq]; occ *= BISHOP_MAGIC_NUMBERS[sq]; occ >>= np.uint64(64-BISHOP_RELEVANT_BITS[sq])
-    return BISHOP_ATTACKS[sq][occ]
-    # return bishop_attacks_on_the_fly(sq, occ)
+    # occ &= BISHOP_MASKS[sq]; occ *= BISHOP_MAGIC_NUMBERS[sq]; occ >>= np.uint64(64-BISHOP_RELEVANT_BITS[sq])
+    # return BISHOP_ATTACKS[sq][occ]
+    return bishop_attacks_on_the_fly(sq, occ)
 
 @numba.njit(numba.uint64(numba.uint8, numba.uint64), cache=True, boundscheck=False, fastmath=True)
 def get_rook_attacks(sq, occ):
     """
     使用魔法位元棋盤查表獲取城堡的攻擊。
     """
-    occ &= ROOK_MASKS[sq]; occ *= ROOK_MAGIC_NUMBERS[sq]; occ >>= np.uint64(64-ROOK_RELEVANT_BITS[sq])
-    return ROOK_ATTACKS[sq][occ]
-    # return rook_attacks_on_the_fly(sq, occ)
+    # occ &= ROOK_MASKS[sq]; occ *= ROOK_MAGIC_NUMBERS[sq]; occ >>= np.uint64(64-ROOK_RELEVANT_BITS[sq])
+    # return ROOK_ATTACKS[sq][occ]
+    return rook_attacks_on_the_fly(sq, occ)
 
 @numba.njit(numba.uint64(numba.uint8, numba.uint64), cache=True, boundscheck=False, fastmath=True)
 def get_queen_attacks(sq, occ): return get_rook_attacks(sq, occ) | get_bishop_attacks(sq, occ)
