bb2c231b3eccafd674d5280419df1eec1ff76ecc
b08203031 <b08203031@ntu.edu.tw>
2025-11-28 16:37:33 +0800

    Passed: 97, Average NPS: 724567, Average search depth: 8.58 關閉see剪枝後能力提升

--- chess_engine/constants.py
+++ chess_engine/constants.py
@@ -406,7 +406,7 @@ DELTA_PRUNING_MARGIN = 500
 
 # --- Static Exchange Evaluation (SEE) Threshold / SEE 閾值 ---
 SEE_THRESHOLD = -50  # centipawns
-ENABLE_SEE_IN_QUIESCENCE = True # Master switch to enable/disable SEE in quiescence search / 啟用/禁用靜態搜尋中 SEE 的總開關
+ENABLE_SEE_IN_QUIESCENCE = False # Master switch to enable/disable SEE in quiescence search / 啟用/禁用靜態搜尋中 SEE 的總開關
 
 # =============================================================================
 # --- Bitboard Utilities Constants / 位元棋盤工具常量 ---
