032da4ffd310307c586b87597c10dbbb7b239025
b08203031 <b08203031@ntu.edu.tw>
2025-12-01 04:23:42 +0800

    還原

--- chess_engine/constants.py
+++ chess_engine/constants.py
@@ -347,7 +347,7 @@ PAWN_PUSH_RANK_BONUS = 2000
 PAWN_PUSH_ATTACK_BONUS = 3000
 
 # History Heuristic Constants
-MAX_HISTORY = 3500 # Max value for history table to prevent overflow and saturation
+MAX_HISTORY = 2048 # Max value for history table to prevent overflow and saturation
 
 # Special value to indicate that the search was stopped due to timeout
 # 特殊值，表示搜尋因超時而停止
